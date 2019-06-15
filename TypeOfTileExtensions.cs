48a3d80ebe9de2ec535522b6e4da2be4166d59b5
bahman <bahmanbit@gmail.com>
2019-06-15 10:50:15 +0430

    all file added

---
+++ TypeOfTileExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace GoogleMapLayer
+{
+    public static class TypeOfTileExtensions
+    {
+
+        public static string TotileString(this TypeOfTile me)
+        {
+            switch (me)
+            {
+                case TypeOfTile.sateliteImage:
+                    return "s@92";
+                case TypeOfTile.map:
+                    return "m@161000000";
+
+                default:
+                    return "t@128";
+            }
+        }
+    }
+}
